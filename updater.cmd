e0b3a8ea111f6f340b78b428ffcf233e4231b044
Plrip <plripcrank@gmail.com>
2018-07-01 17:56:12 +0300

    Update updater.cmd

--- updater.cmd
+++ updater.cmd
@@ -1,6 +1,5 @@
 @Echo Off
 SetLocal EnableExtensions
-taskkill /f /im php.exe
 curl -O https://raw.githubusercontent.com/SteamDatabase/SalienCheat/master/cheat.bat
 curl -O https://raw.githubusercontent.com/SteamDatabase/SalienCheat/master/cheat.php
 curl -O https://raw.githubusercontent.com/SteamDatabase/SalienCheat/master/.gitignore
